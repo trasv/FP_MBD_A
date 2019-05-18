e1783a026a3f5b75803144bf287e1eead8a18fa0
Timo <37039325+trasv@users.noreply.github.com>
2019-05-18 18:25:16 +0700

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,1 +1,17 @@
-
+--jumlah hari sampai terjual
+CREATE OR REPLACE PROCEDURE terjual_hari( idm in date )
+IS
+    temp1 date;
+    temp2 date;
+    hasil date;
+BEGIN
+    SELECT tgl_terjual INTO temp1
+    FROM MOBIL WHERE idm = mobil_id;
+    
+    SELECT tgl_titip INTO temp2
+    FROM PEMBELIAN WHERE idm = mobil_id;
+    
+    hasil := temp1-temp2;
+    
+    DBMS_OUTPUT.PUT_LINE(hasil);
+END
