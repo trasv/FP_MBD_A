f29c921768395e80dfe8bd35156cab0cb17a8837
Timo <37039325+trasv@users.noreply.github.com>
2019-05-18 19:04:39 +0700

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,15 +1,15 @@
 --jumlah hari sampai terjual
-CREATE OR REPLACE PROCEDURE terjual_hari( idm in date )
+CREATE OR REPLACE PROCEDURE terjual_hari( idm in varchar )
 IS
     temp1 date;
     temp2 date;
-    hasil date;
+    hasil int;
 BEGIN
     SELECT tgl_terjual INTO temp1
-    FROM MOBIL WHERE idm = mobil_id;
+    FROM PEMBELIAN WHERE idm = mobil_id;
     
     SELECT tgl_titip INTO temp2
-    FROM PEMBELIAN WHERE idm = mobil_id;
+    FROM MOBIL WHERE idm = mobil_id;
     
     hasil := temp1-temp2;
     
