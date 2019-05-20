14706dd62fe0227bcbacebafe9c4dc887e17b8c7
alfinpradana99 <37216856+alfinpradana99@users.noreply.github.com>
2019-05-20 16:34:20 +0700

    Update sequence.sql

--- sequence.sql
+++ sequence.sql
@@ -1,6 +1,12 @@
---Sequance id tabel pemilik_lama--
+-- All Sequence --
 CREATE SEQUENCE sequence_pemilik_lama nocache;
+CREATE SEQUENCE sequence_pembayaran nocache;
+CREATE SEQUENCE sequence_pembelian nocache;
+CREATE SEQUENCE sequence_pelanggan nocache;
+CREATE SEQUENCE sequence_pegawai nocache;
+create sequence sequence_mobil nocache;
 
+--Auto Increment id tabel pemilik_lama--
 CREATE OR REPLACE TRIGGER auto_pemilik_lama
 BEFORE INSERT ON PEMILIK_LAMA
 FOR EACH ROW
@@ -8,9 +14,7 @@ BEGIN
   SELECT sequence_pemilik_lama.NEXTVAL INTO :NEW.PEMILIK_LAMA_ID FROM DUAL;
 END;
 
---Sequence id tabel pembayaran--
-CREATE SEQUENCE sequence_pembayaran nocache;
-
+-- Auto Increment id tabel pembayaran--
 CREATE OR REPLACE TRIGGER auto_pembayaran
 BEFORE INSERT ON PEMBAYARAN
 FOR EACH ROW
@@ -18,9 +22,7 @@ BEGIN
   SELECT sequence_pembayaran.NEXTVAL INTO :NEW.PEMBAYARAN_ID FROM DUAL;
 END;
 
---Sequence id tabel pembelian--
-CREATE SEQUENCE sequence_pembelian nocache;
-
+-- Auto Increment id tabel pembelian--
 CREATE OR REPLACE TRIGGER auto_pembelian
 BEFORE INSERT ON PEMBELIAN
 FOR EACH ROW
@@ -28,9 +30,7 @@ BEGIN
   SELECT sequence_pembelian.NEXTVAL INTO :NEW.PEMBELIAN_ID FROM DUAL;
 END;
 
---Sequence id tabel pelanggan--
-CREATE SEQUENCE sequence_pelanggan nocache;
-
+-- Auto Increment id tabel pelanggan--
 CREATE OR REPLACE TRIGGER auto_pelanggan
 BEFORE INSERT ON PELANGGAN
 FOR EACH ROW
@@ -38,9 +38,7 @@ BEGIN
   SELECT sequence_pelanggan.NEXTVAL INTO :NEW.PELANGGAN_ID FROM DUAL;
 END;
 
---Sequence id pegawai --
-CREATE SEQUENCE sequence_pegawai nocache;
-
+-- Auto Increment id pegawai --
 CREATE OR REPLACE TRIGGER auto_pegawai
 BEFORE INSERT ON PEGAWAI
 FOR EACH ROW
@@ -48,17 +46,13 @@ BEGIN
   SELECT sequence_pegawai.NEXTVAL INTO :NEW.PEGAWAI_ID FROM DUAL;
 END;
 
---Sequence id mobil--
-create sequence sequence_mobil nocache;
-
+-- Auto Increment id mobil--
 create or replace trigger auto_mobil
 before insert on mobil
 for each row
 declare depan varchar(50);
 begin
-  select SUBSTR(:new.tipe,1,3)
-  into depan
-  from mobil;
+  depan := SUBSTR(:new.tipe,1,3);
   select depan || to_char(sequence_mobil.NEXTVAL,'FM00000')
   into :NEW.mobil_id
   from DUAL;
